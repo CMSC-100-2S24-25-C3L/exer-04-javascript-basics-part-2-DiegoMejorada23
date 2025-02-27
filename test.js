dbeaeff475b4dc720a0a124f9b8779d46015dbae
DiegoMejorada23 <dvmejorada@up.edu.ph>
2025-02-27 15:03:11 +0800

    CHORE: updated and added test cases in test.js

--- test.js
+++ test.js
@@ -4,3 +4,23 @@
 // February 27, 2025
 // Exer 04: JavaScript Basics Part 2
 //  Simulate a register program in JavaScript (tests file)
+
+import { addAccount } from './index.js';
+
+console.log(addAccount(["Alan"]));                                      // Returns false
+
+console.log(addAccount(["Alan", "Turing"]));                            // Returns false
+
+console.log(addAccount(["Alan", "Turing", "aturing@w3c.com"]));         // Returns false
+
+console.log(addAccount(["Alan", "Turing", "aturing@w3c.com", 58]));     // Returns true
+
+console.log(addAccount(["", "Turing", "aturing@w3c.com", 58]));         // Returns false
+
+console.log(addAccount(["Alan", "", "aturing@w3c.com", 58]));           // Returns false
+
+console.log(addAccount(["Alan", "Turing", "", 58]));                    // Returns false
+
+console.log(addAccount(["Alan", "Turing", "aturing@w3c.com", 17]));     // Returns false
+
+console.log(addAccount(["Alan", "Turing", "aturing", 58]));             // Returns false
