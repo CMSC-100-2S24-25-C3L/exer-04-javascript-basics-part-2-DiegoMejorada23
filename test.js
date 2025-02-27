42dc9a98fdef9de0c7919f02b0201669fd93af57
DiegoMejorada23 <dvmejorada@up.edu.ph>
2025-02-27 15:05:26 +0800

    CHORE: added more test cases in test.js

--- test.js
+++ test.js
@@ -7,20 +7,22 @@
 
 import { addAccount } from './index.js';
 
-console.log(addAccount(["Alan"]));                                      // Returns false
+console.log(addAccount(["Alan"]));                                              // Returns false
 
-console.log(addAccount(["Alan", "Turing"]));                            // Returns false
+console.log(addAccount(["Alan", "Turing"]));                                    // Returns false
 
-console.log(addAccount(["Alan", "Turing", "aturing@w3c.com"]));         // Returns false
+console.log(addAccount(["Alan", "Turing", "aturing@w3c.com"]));                 // Returns false
 
-console.log(addAccount(["Alan", "Turing", "aturing@w3c.com", 58]));     // Returns true
+console.log(addAccount(["Alan", "Turing", "aturing@w3c.com", 58]));             // Returns true
 
-console.log(addAccount(["", "Turing", "aturing@w3c.com", 58]));         // Returns false
+console.log(addAccount(["", "Turing", "aturing@w3c.com", 58]));                 // Returns false
 
-console.log(addAccount(["Alan", "", "aturing@w3c.com", 58]));           // Returns false
+console.log(addAccount(["Alan", "", "aturing@w3c.com", 58]));                   // Returns false
 
-console.log(addAccount(["Alan", "Turing", "", 58]));                    // Returns false
+console.log(addAccount(["Alan", "Turing", "", 58]));                            // Returns false
 
-console.log(addAccount(["Alan", "Turing", "aturing@w3c.com", 17]));     // Returns false
+console.log(addAccount(["Alan", "Turing", "aturing@w3c.com", 17]));             // Returns false
 
-console.log(addAccount(["Alan", "Turing", "aturing", 58]));             // Returns false
+console.log(addAccount(["Alan", "Turing", "aturing", 58]));                     // Returns false
+
+console.log(addAccount(["Diego", "Mejorada", "dvmejorada@up.edu.ph", 20]));     // Returns true
